3857a03e85351ed11dd193f4f89d5ff3df0bf2ba
Jake <jake@steamclock.com>
2017-11-16 12:42:11 -0800

    Add display activity

---
+++ sanitychecker/src/main/java/com/melonhead/sanitychecker/JSONDisplayActivity.kt
@@ -0,0 +1,35 @@
+package com.melonhead.sanitychecker
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import io.github.kbiakov.codeview.adapters.Format
+import io.github.kbiakov.codeview.adapters.Options
+import io.github.kbiakov.codeview.classifier.CodeProcessor
+import io.github.kbiakov.codeview.highlight.ColorTheme
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
+import kotlinx.coroutines.experimental.android.UI
+import kotlinx.coroutines.experimental.async
+
+abstract class JSONDisplayActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        CodeProcessor.init(this)
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        code_view.setOptions(Options.Default.get(this)
+                .withFormat(Format.ExtraCompact)
+                .withTheme(ColorTheme.MONOKAI)
+        )
+
+        fab.setOnClickListener { _ ->
+            async(UI) {
+                runTests()
+            }
+        }
+    }
+
+    abstract fun runTests()
+
+}
