90e81b7dbb4c8ba7911a56f5d51a9675dc59f4c7
Jake <jake@steamclock.com>
2017-11-16 12:26:35 -0800

    Clean up objects

--- sanitychecker/src/main/java/com/melonhead/sanitychecker/SanityChecker.kt
+++ sanitychecker/src/main/java/com/melonhead/sanitychecker/SanityChecker.kt
@@ -1,25 +1,17 @@
 package com.melonhead.sanitychecker
 
+import org.json.JSONObject
 import retrofit2.Call
 
 /**
  * serversanity
  * Created by jake on 2017-11-15, 2:30 PM
  */
-
+data class ApiResult(var url: String, var success: Boolean, var json: JSONObject)
 
 object SanityChecker {
-    suspend fun runTests(vararg calls: APICall<*>): JSONObjects {
-        val jsonObjs = JSONObjects(emptyList(), emptyList())
-
-        for (call in calls) {
-            val server = Server(call)
-            val (success, failed) = server.test()
-            jsonObjs.success += success
-            jsonObjs.failed += failed
-        }
-
-        return jsonObjs
+    suspend fun runTests(vararg calls: APICall<*>): List<ApiResult> {
+        return calls.map { Server(it).test() }
     }
 }
 
